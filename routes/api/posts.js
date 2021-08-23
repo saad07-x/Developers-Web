2e52d672d55830fe5a9542c8a25151bf8037a6d3
saad07-x <mirsaad11@hotmail.com>
2021-08-23 19:49:02 +0500

    midway through

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const router = express.Router();
+// const express = require('express');
+// const router = express.Router();
 
 
-//@route    GET api/posts
-//@desc     Test Route
-//@access   Public - By public it means you don't need any token to access this. (e.g. no password required)
-router.get('/', (req,res) => res.send('Post Route'))
+// //@route    GET api/posts
+// //@desc     Test Route
+// //@access   Public - By public it means you don't need any token to access this. (e.g. no password required)
+// router.get('/', (req,res) => res.send('Post Route'))
 
 
-module.exports = router;
+// module.exports = router;
